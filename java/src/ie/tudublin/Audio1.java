ae4d70e96e999b0b47ce227627c424076bd36aac
Sanat <sanat.thukral@gmail.com>
2021-03-11 09:51:31 +0000

    master updated

--- java/src/ie/tudublin/Audio1.java
+++ java/src/ie/tudublin/Audio1.java
@@ -13,62 +13,71 @@ public class Audio1 extends PApplet {
     AudioPlayer ap;
     AudioBuffer ab; // Samples
 
-    float[] lerpBuffer;
+    float[] lerpedBuffer;
 
-    public void settings()
-    {
+    public void settings() {
         size(512, 512);
         // fullScreen(P3D, SPAN); // Try this for full screen multiple monitor support :-) Be careful of exceptions!
     }
 
-    float y = 300;
+    float y = 200;
     float lerpedY = y;
 
+    int which = 0;
 
-    public void setup()
-    {
+    public void setup() {
         minim = new Minim(this);
         ai = minim.getLineIn(Minim.MONO, width, 44100, 16);
         //ap = minim.loadFile("heroplanet.mp3", width);
         //ab = ap.mix; // Connect the buffer to the mp3 file
         ab = ai.mix; 
         colorMode(HSB);
-        lerpBuffer = new float[width];
+        lerpedBuffer = new float[width];
 
     }
 
-    float lerpedAverage;
+    public void keyPressed() {
+        if (keyCode >= '0' && keyCode <= '5') {
+            which = keyCode - '0';
+        }
+        if (keyCode == ' ') {
+            if (ap.isPlaying()) {
+                ap.pause();
+            } else {
+                ap.rewind();
+                ap.play();
+            }
+        }
+    }
 
-    public void draw()
-    {
+    float lerpedAverage = 0;
+
+    public void draw() {
         background(0);
         stroke(255);
         float halfHeight = height / 2;
         float average = 0;
         float sum = 0;
-        for(int i = 0 ; i < ab.size(); i ++)
+
+        // Calculate the average of the buffer
+        for (int i = 0; i < ab.size(); i ++)
         {
-            float c = map(i, 0, ab.size(), 0, 255);
-            stroke(c, 255, 255);
-            lerpBuffer[i] = lerp(lerpBuffer[i], ab.get(i),0.1f);
-
-            line(i, halfHeight - lerpBuffer[i] * halfHeight, halfHeight + lerpBuffer[i] * halfHeight, i);
-            sum = (abs(ab.get(i)) + sum);
-            
-            //println(ab.get(i));
+            sum += abs(ab.get(i));
         }
-        // calculate the average amplitude
-        average = sum / (float)ab.size();
+        average = sum / ab.size();
+        // Move lerpedAverage 10% closer to average every frame
         lerpedAverage = lerp(lerpedAverage, average, 0.1f);
-        //lerped eclipse
-        // ellipse(width / 2, 100, 50 + (lerpedAverage * 500), 50 + (lerpedAverage * 500));
-        // Non lerped eclipse
-        // ellipse(width / 4, 100, average * 500, average * 500);
 
-        // ellipse(200, y, 30, 30);
-        // ellipse(300, lerpedY, 30, 30);
-        // y += random(-10,10);
-        // lerpedY = lerp(lerpedY,y,0.1f);
+        switch (which)
+        {
+            case 0:
+            {
+                // Iterate over all the elements in the audio buffer
+                for (int i = 0; i < ab.size(); i++) {
+
+                    float c = map(i, 0, ab.size(), 0, 255);
+                    stroke(c, 255, 255);
+                    lerpedBuffer[i] = lerp(lerpedBuffer[i], ab.get(i), 0.1f);
         
                     line(i, halfHeight - lerpedBuffer[i] * halfHeight * 4, halfHeight + lerpedBuffer[i] * halfHeight * 4, i);
                 }        
